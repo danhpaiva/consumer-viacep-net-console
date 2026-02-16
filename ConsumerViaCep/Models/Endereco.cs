506bb42166e9060a6bf7fee44021cd1314226546
Paiva <danpaiva@live.com>
2026-02-16 16:52:34 -0300

    feat: add logica

--- ConsumerViaCep/Models/Endereco.cs
+++ ConsumerViaCep/Models/Endereco.cs
@@ -2,16 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace ConsumerViaCep.Models
 {
     public class Endereco
     {
+        [JsonPropertyName("cep")]
         public string Cep { get; set; }
+
+         [JsonPropertyName("logradouro")]
         public string Logradouro { get; set; }
+
         public string Complemento { get; set; }
+
+         [JsonPropertyName("bairro")]
         public string Bairro { get; set; }
+
+         [JsonPropertyName("localidade")]
         public string Localidade { get; set; }
+
+        [JsonPropertyName("uf")]
         public string Uf { get; set; }
         public string Ibge { get; set; }
         public string Gia { get; set; }
--- ConsumerViaCep/Program.cs
+++ ConsumerViaCep/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Digite o CEP para consulta:");
+string cep = ReadLine();
+
+var enderecoUrl = $"https://viacep.com.br/ws/{cep}/json/";
+
+WriteLine($"Consultando o endereço para o CEP: {cep}...");
+
+var httpClient = new HttpClient();
+
+try
+{
+  HttpResponseMessage? response = await httpClient.GetAsync(enderecoUrl);
+  response.EnsureSuccessStatusCode();
+
+  string responseBody = await response.Content.ReadAsStringAsync();
+  
+  var endereco = System.Text.Json.JsonSerializer.Deserialize<ConsumerViaCep.Models.Endereco>(responseBody);
+  WriteLine($"Endereço encontrado: {endereco.Logradouro}, {endereco.Bairro}, {endereco.Localidade} - {endereco.Uf}");
+}
+catch (System.Exception)
+{
+  WriteLine("Ocorreu um erro ao consultar o endereço. Verifique o CEP e tente novamente.");
+}
